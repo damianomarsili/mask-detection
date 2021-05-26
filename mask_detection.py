c28de25de0d4ee2bce15b7410c306aef4e0c8fa0
Damiano Marsili <dmarsil1@jh.edu>
2021-05-25 20:22:32 -0400

    Implemented detect_mask and completed main()

--- mask_detection.py
+++ mask_detection.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 import os
 from face_detection import detect_faces, print_face, crop_image
 
+# Loads model from files
 def load_model(json, weights):
     # Load json and create model
     json_file = open(json, 'r')
@@ -14,6 +15,22 @@ def load_model(json, weights):
     # Load weights into model
     model.load_weights(weights)
     return model
+
+# Applies image smoothing
+def blur_image(image, image_size):
+    image = cv2.blur(image, (5,5))
+    image = cv2.resize(image, image_size)
+    return image
+
+# TODO: Check all shapes/sizes are correct. - Might have to normalize the sizes but tbh not sure
+def detect_mask(image, image_size, model):
+    x = []
+    image_size = (image_size, image_size)
+    image = blur_image(image, image_size)
+    x.append(image)
+    image = np.array(x)
+    return model.predict(image) > 0.5
+
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -34,8 +51,11 @@ def main():
         for face in faces:
             # print_face(image, face, True) # Can uncomment to see facial detection
             cropped_image = crop_image(image, face)
-            
-            # TODO: send cropped_image to model to evaluate 
+            h, w = cropped_image.shape
+            if detect_mask(image, (h,w), model):
+                print_face(image, face, True) 
+            else:
+                print_face(image, face, False)
 
         if cv2.waitKey(5) & 0xFF == 27:
             break
--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import cv2 
+import numpy as np
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
 def save_model(json_filename, weights_filename, model):
@@ -13,3 +16,9 @@ def save_model(json_filename, weights_filename, model):
     model.save_weights(weights_filename)
 
 
+def train():
+    pass
+
+
+model = train()
+save_model('mask_detetion.json', 'mask_detection.h5', model)
